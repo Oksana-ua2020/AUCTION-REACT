641ff07564e9d545c73d86d177d8051d0cf6ad07
Oksana Ratova <oksana.ratov@gmail.com>
2023-10-19 16:37:28 +0300

    add new commponents title

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -30,30 +30,5 @@ function Main() {
     );
 }
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let object__product = {
-    image: "https://picsum.photos/200/300",
-    number:  1,
-    price: 128,
-}
-function selectQuantity(select) {
-    console.log(randomNumber);
-    let Quantity = Number(select);
-    let result = "";
-
-    for (let i = 1; i < Quantity+1; i++) {
-        let price = Math.floor(Math.random() * (100 - 1)) + 1;
-        result += `
-            <div className="pic">
-                <div className="pic-position">
-                    <img src = "${object__product.image}" alt="product">
-                </div>
-                <p>Price ${price} $</p>
-            </div>`;
-    }
-    document.getElementById('pic-img').innerHTML = result;
-}
-
-selectQuantity(10);
 
 export default Main;
